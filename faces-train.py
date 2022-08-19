228ac97a58e05395fc8201d2b2bee0043d270d6c
CaioVGA <caio.v.araujo1997@hotmail.com>
2022-08-19 00:48:32 -0300

    Atualização do código.

--- faces-train.py
+++ faces-train.py
@@ -5,17 +5,20 @@ import pickle
 import cv2
 import numpy as np
 
+
+# mapeamento dos diretórios com amostras de imagens
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 image_dir = os.path.join(BASE_DIR, "images")
 
+# importação dos kernels haar para a classificação
 face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
 recognizer = cv2.face.LBPHFaceRecognizer_create()
 
 
 current_id_number = 0
-label_ids = dict()
-x_train = list()
-y_labels = list()
+label_ids = dict() # id de cada usuário
+x_train = list() # dados reservados para o treinamento do modelo
+y_labels = list() # rótulo de identificação
 
 for root, dirs, files in os.walk(image_dir):
     for file in files:
@@ -26,27 +29,21 @@ for root, dirs, files in os.walk(image_dir):
                 label_ids[label] = current_id_number
                 current_id_number += 1
             id_ = label_ids[label]
-            # verifica a imagem em questão, transforma em uma lista do tipo numpy (gray scale).
-            # x_train.append(path) 
-            # y_labels.append(label)
-            pil_image = Image.open(path).convert("L") # grayscale
+            pil_image = Image.open(path).convert("L") 
             size = (550, 550)
             final_image = pil_image.resize(size, Image.Resampling.LANCZOS)
             image_array = np.array(final_image, "uint8")
-            # print(image_array)
             faces = face_cascade.detectMultiScale(image_array, scaleFactor=1.5, minNeighbors=5)
 
+            # delimita a região de interesse de identificação
             for (x, y, w, h) in faces:
                 roi = image_array[y:y+h, x:x+w]
                 x_train.append(roi)
                 y_labels.append(id_)
 
-# print(y_labels)
-# print(x_train)
-
 with open("labels.pickle", 'wb') as file:
     pickle.dump(label_ids, file)
 
-# Training the model with machine learning method
+# treinamento do modelo a partir dos dados coletados e representação do mesmo
 recognizer.train(x_train, np.array(y_labels))
-recognizer.save("trainning.yml") # save as a file
+recognizer.save("trainning.yml") # output: trainning.yml
